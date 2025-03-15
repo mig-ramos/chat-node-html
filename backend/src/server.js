3b6dac0ee5edeacb3bf9f042c0fbaac57bce9954
Miguel Arcanjo <mig.ramos10@gmail.com>
2025-03-14 22:31:50 -0300

    ajustes finais

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,6 +9,7 @@ wss.on("connection", (ws) => {
   ws.on("error", console.error);
 
   ws.on("message", (data) => {
+    // console.log(data.toString());
     wss.clients.forEach((client) => client.send(data.toString()));
     ws.send(data);
   });
